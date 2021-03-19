11211ad4fa6a4b6ab254ae16e07890e3d93af8c2
dataininstitute <68183539+dataininstitute@users.noreply.github.com>
2021-03-19 10:24:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from streamlit_pandas_profiling import st_profile_report
 
 st.title("Analyser votre Dataset avec Pandas-profiling")
 st.subheader("Insérer votre Dataset au format csv, Pandas-profiling s'occupe de l'analyser !")
-st.write("""Pour chaque colonne, les statistiques suivantes - si elles sont pertinentes pour le type de colonne - sont présentées dans un rapport HTML interactif: \n
+st.markdown("""Pour chaque colonne, les statistiques suivantes - si elles sont pertinentes pour le type de colonne - sont présentées dans un rapport HTML interactif :
 
-	- Inférence de type: détecte les types de colonnes dans un dataframe. \n
-	Essentiels: type, valeurs uniques, valeurs manquantes \n
-	Statistiques quantiles telles que valeur minimale, Q1, médiane, Q3, maximum, intervalle, intervalle interquartile \n
-	Statistiques descriptives comme la moyenne, le mode, l'écart type, la somme, l'écart absolu médian, le coefficient de variation, l'aplatissement, l'asymétrie
-	Valeurs les plus fréquentes \n
-	Histogramme \n
-	Corrélations mettant en évidence des variables hautement corrélées, matrices de Spearman, Pearson et Kendall \n
-	Matrice des valeurs manquantes, nombre, carte thermique et dendrogramme des valeurs manquantes \n
-	L'analyse de texte apprend les catégories (majuscules, espace), les scripts (latin, cyrillique) et les blocs (ASCII) de données textuelles. \n
-	L'analyse des fichiers et des images extrait la taille des fichiers, les dates de création et les dimensions et recherche les images tronquées ou celles contenant des informations EXIF.""")
+- **Inférence de type**: détecte les types de colonnes dans un dataframe. 
+- **Essentiels**: type, valeurs uniques, valeurs manquantes 
+- Statistiques quantiles telles que valeur minimale, Q1, médiane, Q3, maximum, intervalle, intervalle interquartile 
+- Statistiques descriptives comme la moyenne, le mode, l'écart type, la somme, l'écart absolu médian, le coefficient de variation, l'aplatissement, l'asymétrie
+- Valeurs les plus fréquentes 
+- Histogramme 
+- Corrélations mettant en évidence des variables hautement corrélées, matrices de Spearman, Pearson et Kendall 
+- Matrice des valeurs manquantes, nombre, carte thermique et dendrogramme des valeurs manquantes 
+- L'analyse de texte apprend les catégories (majuscules, espace), les scripts (latin, cyrillique) et les blocs (ASCII) de données textuelles. 
+- L'analyse des fichiers et des images extrait la taille des fichiers, les dates de création et les dimensions et recherche les images tronquées ou celles contenant des informations EXIF.""")
 
 file_upload = st.file_uploader("Uploader votre csv à analyser", type=["csv"])
 #file_upload = st.file_uploader("Uploader votre json à analyser", type=["json"])
